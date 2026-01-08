0de3003548d406a5a63dc94c9cdf1cee7edc32d8
ayushhhhhh-code <ayushbhoir48@gmail.com>
2026-01-08 10:26:12 +0000

    variables and constants basic done

---
+++ 01_basic/variables.js
@@ -0,0 +1,11 @@
+const accountId = 93647734
+let accountEmail = "ayushbhoir98@gmail.com"
+var accountPass = "40750437"
+accountcity = "mumbai"
+let accountState;
+// const cannot be changed 
+console.table([accountId,accountEmail,accountPass,accountcity,accountState]);
+
+/*
+prefer not to use var because of issue with old times javascript
+*/
