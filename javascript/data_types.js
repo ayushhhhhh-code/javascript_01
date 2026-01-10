a11421a57a1bc46eb848c4abf524a3457f3057ea
ayushhhhhh-code <ayushbhoir48@gmail.com>
2026-01-10 11:50:38 +0000

    Add data types demonstration in JavaScript

---
+++ javascript/data_types.js
@@ -0,0 +1,35 @@
+"use strict";   // to avoid undeclared variables
+//treat all javascript code as modern
+
+//alert(3+4)   //won't work in nodejs works in browser
+
+// code readability should be there
+
+// Data types in javascript
+
+let number = 45
+let string = "AYUSH"
+let loggedIn = true
+let state = null
+let notAssigned;
+let object = {name: "ayush", age: 22}
+let uniqueId = Symbol("id")
+//Number = 2 to power 53  limit
+//string = "hello world"
+//boolean = true/false
+//null = empty value
+//undefined = value not assigned
+//object = {name: "ayush", age: 22}
+//symbol = unique 
+
+
+
+console.log(typeof number)
+console.log(typeof string)
+console.log(typeof loggedIn)
+console.log(typeof state)
+console.log(typeof notAssigned)
+console.log(typeof object)
+console.log(typeof uniqueId)
+
+//JS is dynamically typed language
